4dee2cf588f80d1f9de87ec1d12f7f8ab8a6bde7
sinvan <muhammedsinvan3@gmail.com>
2023-12-20 13:36:47 +0530

    rightbar added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SideBar from './Components/SideBar/SideBar';
 import Header from './Components/Header/Header';
 import Dashboard from './Components/Dashboard/Dashboard';
+import RightBar from './Components/RightBar/RightBar';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Dashboard />
      </div>
      <div className='app_rightContainer'>
-      
+      <RightBar />
      </div>
      </div>
     </div>
---
+++ src/Components/RightBar/RightBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./RightBar.css";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AddIcon from '@mui/icons-material/Add';
+
+const RightBar = () => {
+  return (
+    <div className='rightBar_container'>
+        <div className='rightBar_container-notification'>
+        <NotificationsIcon  className='rightBar_container-notificationIcon'/>
+        </div>
+        <div className='rightBar_container-message' />
+
+        <div className='rightBar_container-add'>
+            <AddIcon  className='rightBar_container-addIcon'/>
+        </div>
+  
+    </div>
+  )
+}
+
+export default RightBar
